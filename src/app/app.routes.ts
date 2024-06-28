1afde134a0869133badcdcad85aa3ef0c3636842
Yavuz <devyavuz81@gmail.com>
2024-06-28 18:19:08 +0300

    RouteAyarlandi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ArticlesComponent } from '../Screens/articles/articles.component';
+import { AboutUsComponent } from '../Screens/about-us/about-us.component';
+import { ProjectsComponent } from '../Screens/projects/projects.component';
+import { ContactComponent } from '../Screens/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'articles', component: ArticlesComponent },
+    { path: 'about-us', component: AboutUsComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'contact', component: ContactComponent }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ArticlesComponent } from './Screens/articles/articles.component';
+import { AboutUsComponent } from './Screens/about-us/about-us.component';
+import { ProjectsComponent } from './Screens/projects/projects.component';
+import { ContactComponent } from './Screens/contact/contact.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+const routes: Routes = [
+  { path: 'articles', component: ArticlesComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent }
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(BrowserModule)
+  ]
+})
+.catch(err => console.error(err));
